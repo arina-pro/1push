4996c655136ceed53c4653a32aed697be5db9837
Arina Pro <arina-pro@yandex.ru>
2019-12-14 19:50:40 +0300

    minor fix 0

--- one_push_bot.py
+++ one_push_bot.py
@@ -18,7 +18,7 @@ bot = telebot.TeleBot(TOKEN)
 def send_welcome(message):
 	bot.reply_to(message, "Type /start to have see events\nType \'player\' to see stats")
 
-chat_id = bot.get_updates()[-1].message.chat.id
+#chat_id = bot.get_updates()[-1].message.chat.id
 
 #@bot.message_handler(func=lambda message: True)
 @bot.message_handler(regexp="player")
@@ -26,7 +26,7 @@ def handle_message(message):
 	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=2, selective=False)
 	for i in range(10):
 		markup.add(types.KeyboardButton(str(i)))
-	bot.send_message(chat_id, "Choose a player:", reply_markup=markup)
+	bot.send_message(message.chat.id, "Choose a player:", reply_markup=markup)
 	@bot.message_handler(regexp="\d")
 	def handle_inner_message(msg):
 		player_num =  int(float(msg.text))
@@ -36,24 +36,27 @@ def handle_message(message):
 		Score = response["RoundEndState"]["Players"][player_num]["AdditionalInfo"]["StartScore"]
 		Kills = response["RoundEndState"]["Players"][player_num]["AdditionalInfo"]["Kills"]
 		MVPs = response["RoundEndState"]["Players"][player_num]["AdditionalInfo"]["MVPs"]
-		Money = response["RoundEndState"]["Players"][player_num]["Money"]
+		Money = response["RoundStartState"]["Players"][player_num]["Money"]
+		MoneyLeft = response["RoundEndState"]["Players"][player_num]["Money"]
 		
-		bot.send_message(chat_id, "Kills in previous rounds: " + str(Score))
-		bot.send_message(chat_id, "Kills: " + str(Kills))
-		bot.send_message(chat_id, "Assists: " + str(Assists))
-		bot.send_message(chat_id, "Deaths: " + str(Deaths))
-		bot.send_message(chat_id, "MVPs: " + str(MVPs))
-		bot.send_message(chat_id, "Money: " + str(Money))
+		bot.send_message(message.chat.id, "Kills in previous rounds: " + str(Score))
+		bot.send_message(message.chat.id, "Kills: " + str(Kills))
+		bot.send_message(message.chat.id, "Assists: " + str(Assists))
+		bot.send_message(message.chat.id, "Deaths: " + str(Deaths))
+		bot.send_message(message.chat.id, "MVPs: " + str(MVPs))
+		bot.send_message(message.chat.id, "Money: " + str(Money))
+		bot.send_message(message.chat.id, "Money left: " + str(MoneyLeft))
+
 
 @bot.message_handler(commands=['start'])
 def echo_all(message):
 	for i in range(0, 2981):
-		URL = "http://486f82fb.ngrok.io/get-event"
+		URL = "http://7a6066b9.ngrok.io/get-event"
 		PARAMS = {'frame': i}
 		r = requests.get(url = URL, params = PARAMS) 
 		data = r.json()
 		for j in range(len(data)):
-			bot.send_message(chat_id, 'FrameNumber: ' + str(data[j]['FrameNumber']) + '\nEventType: ' + dic[str(data[j]['EventType']) if int(data[j]['EventType']) < 67 else '0'])
+			bot.send_message(message.chat.id, 'FrameNumber: ' + str(data[j]['FrameNumber']) + '\nEventType: ' + dic[str(data[j]['EventType']) if int(data[j]['EventType']) < 67 else '0'])
 			# Specify weapon! str(data[j]['Data']['Weapon']
 			#bot.send_message(chat_id, 'EventType: ' + dic[str(data[j]['EventType']) if int(data[j]['EventType']) < 67 else '0'])
 			print(data[j])
